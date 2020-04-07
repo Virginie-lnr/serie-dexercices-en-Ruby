c8f55ce7f44aacf8d9a66e4a785c330a9a5901a5
Virginie <virginie.lenoir17@gmail.com>
2020-04-07 21:54:02 +0200

    ajout de la pyramide exo20

--- exo_20.rb
+++ exo_20.rb
@@ -1,3 +1,27 @@
-puts "Peux-tu me donner un nombre entre 1 et 25?"
+# Construis un programme exo_20.rb qui va demander à l'utilisateur un nombre entre 1 et 25 et qui va sortir une pyramide à descendre de tant d'étages que ce nombre. Voici un exemple :
+# Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?
+# > 5
+# Voici la pyramide :
+
+#
+##
+###
+####
+#####
+
+
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 print "> "
-number = gets.chomp.to_i
+etages = gets.chomp.to_i
+
+numberOfHash = 1
+
+while numberOfHash < etages 
+    if etages >= 25 && etages <= 1
+        puts "Erreur, choisissez un nombre entre 1 et 25"
+    else
+    etages.times
+    puts "# " * numberOfHash
+    numberOfHash += 1
+    end
+end
