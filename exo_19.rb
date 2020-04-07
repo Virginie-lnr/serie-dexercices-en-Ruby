42b2cf52bef4dc4a75e3f414802c745c606621d8
Virginie <virginie.lenoir17@gmail.com>
2020-04-07 20:16:14 +0200

    ajout de la consigne en commentaire

--- exo_19.rb
+++ exo_19.rb
@@ -1,3 +1,9 @@
+=begin 
+Prends le programme exo_18.rb et créé un programme exo_19.rb qui va reprendre 
+l'array des emails créés, et n'afficher que les emails avec un nombre pair.
+=end 
+
+
 number = 0 
 numberY = 0
 arrayIterator = 0
